93ecd6d49fa9dc56968dc485dd8c90752468640a
Daniel Freeman <Daniel@daniels-macbook-pro.local>
2015-11-24 13:56:03 +1100

    Initial Commit

---
+++ EWB Design SummitTests/EWB_Design_SummitTests.swift
@@ -0,0 +1,36 @@
+//
+//  EWB_Design_SummitTests.swift
+//  EWB Design SummitTests
+//
+//  Created by Daniel Freeman on 24/11/2015.
+//  Copyright © 2015 Engineers Without Borders. All rights reserved.
+//
+
+import XCTest
+@testable import EWB_Design_Summit
+
+class EWB_Design_SummitTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
