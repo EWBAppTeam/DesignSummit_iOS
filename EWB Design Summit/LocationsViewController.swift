fd1866313cda0915131eb5ff3ee9dcc3cc30e1a0
Daniel Freeman <Daniel@daniels-macbook-pro.local>
2015-12-21 15:55:46 +1100

    Most uptp Date Version: Includes Login and Register, Viewing PDF on Info Screen

--- EWB Design Summit/LocationsViewController.swift
+++ EWB Design Summit/LocationsViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import MapKit
 
 class LocationsViewController: UIViewController {
 
@@ -16,6 +17,8 @@ class LocationsViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
