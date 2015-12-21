0bee1ef01dc903bf8ec70a0812dc919fa05f2356
Daniel Freeman <Daniel@daniels-macbook-pro.local>
2015-12-21 12:39:39 +1100

    Loads a PDF directly from the device using NDBundle.mainBundle() Next step is to load a PDF from the web and save to the device. then: - if able to connect, check if up to date, if so load, if not redownload new version and save. 	if can't connect, then load current and display message.

--- EWB Design Summit/InfoViewController.swift
+++ EWB Design Summit/InfoViewController.swift
@@ -17,20 +17,41 @@ class InfoViewController: UIViewController {
     
     override func loadView() {
         print("loadView")
-        
         super.loadView()
         
         self.webView = WKWebView()
         self.view = self.webView!
+        
     }
     
     override func viewDidLoad() {
         print("View did Load")
         super.viewDidLoad()
         
+        if let myPDF = NSBundle.mainBundle().URLForResource("DS Jan2016 Info", withExtension: "pdf"){
+            
+            if NSFileManager.defaultManager().fileExistsAtPath(myPDF.path!) {
+                print("file found")
+                print("at \(myPDF.path!)")
+            }
+            
+            let myURLReq = NSURLRequest(URL: myPDF)
+            self.webView!.loadRequest(myURLReq)
+        }
+        
+        
+        
+       
+        
+        
+        
+        // Code for loading PDF from Box or website in general
+        /*
+       
         let url = NSURL(string:"https://ewb.box.com/s/wja5zji87n4wpe7xy4pzaqlno4km4r5y")
         let req = NSURLRequest(URL:url!)
         self.webView!.loadRequest(req)
+        */
     }
     
     override func didReceiveMemoryWarning() {
