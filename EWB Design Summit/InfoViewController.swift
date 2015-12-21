d6d7209b72a256db139d284ba1625b8f922703d0
Daniel Freeman <Daniel@daniels-macbook-pro.local>
2015-12-21 11:43:36 +1100

    Info Screen loads a static link from Box It is using a WebKit and WKWebView

--- EWB Design Summit/InfoViewController.swift
+++ EWB Design Summit/InfoViewController.swift
@@ -7,29 +7,34 @@
 //
 
 import UIKit
+import WebKit
 
 class InfoViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @IBOutlet var containerView: UIView!
+    var webView: WKWebView?
+    
+    
+    override func loadView() {
+        print("loadView")
+        
+        super.loadView()
+        
+        self.webView = WKWebView()
+        self.view = self.webView!
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    override func viewDidLoad() {
+        print("View did Load")
+        super.viewDidLoad()
+        
+        let url = NSURL(string:"https://ewb.box.com/s/wja5zji87n4wpe7xy4pzaqlno4km4r5y")
+        let req = NSURLRequest(URL:url!)
+        self.webView!.loadRequest(req)
     }
-    */
-
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
 }
