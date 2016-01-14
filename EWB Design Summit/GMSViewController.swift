69e074e260a0956f78de1fef8f096582aca00186
Daniel Freeman <Daniel@daniels-macbook-pro.local>
2016-01-14 15:56:41 +1100

    Google Maps Places

--- EWB Design Summit/GMSViewController.swift
+++ EWB Design Summit/GMSViewController.swift
@@ -16,38 +16,28 @@ class GMSViewController: UIViewController {
         
         
         
-        let camera = GMSCameraPosition.cameraWithLatitude(-33.86,
-            longitude: 151.20, zoom: 11, bearing: 0, viewingAngle:0)
+        let camera = GMSCameraPosition.cameraWithLatitude(-11.55539,
+            longitude: 104.92473, zoom: 11, bearing: 0, viewingAngle:0)
         
         let mapView = GMSMapView.mapWithFrame(CGRectZero, camera: camera)
         mapView.myLocationEnabled = true
         mapView.settings.myLocationButton = true
         mapView.settings.compassButton = true
         
-        
-        let marker = GMSMarker()
-        marker.position = CLLocationCoordinate2DMake(-33.86, 151.20)
-        marker.title = "Sydney"
-        marker.snippet = "Australia"
-        marker.icon = UIImage(named: "house")
-        marker.map = mapView
-        
-        let marker2 = GMSMarker()
-        marker2.position = CLLocationCoordinate2DMake(-27.86, 154.20)
-        marker2.title = "Melbourne"
-        marker2.snippet = "The best place to live in the world"
-        marker2.map = mapView
+    
+
         
         let AniseHotelandRestaurant = GMSMarker()
         AniseHotelandRestaurant.position = CLLocationCoordinate2DMake(11.55539, 104.92473)
-        marker2.title = "AniseHotelandRestaurant"
-        marker2.snippet = "Meeting Point"
+        AniseHotelandRestaurant.title = "AniseHotelandRestaurant"
+        AniseHotelandRestaurant.snippet = "Meeting Point"
         AniseHotelandRestaurant.map = mapView
         
         let TopBananaHotel = GMSMarker()
         TopBananaHotel.position = CLLocationCoordinate2DMake(11.55546, 104.9261)
         TopBananaHotel.title = "AniseHotelandRestaurant"
         TopBananaHotel.snippet = "Meeting Point"
+        TopBananaHotel.icon = UIImage(named: "house")
         TopBananaHotel.map = mapView
         
         
--- FeedbackViewController.swift
+++ FeedbackViewController.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import WebKit
+import Foundation
 
 class FeedbackViewController: UIViewController {
     
@@ -50,21 +51,88 @@ class FeedbackViewController: UIViewController {
         
 
 */
+      
         
+        // Using a Session with cachePolicy
         
         let url = NSURL(string: "https://docs.google.com/forms/d/1vif6hhJ6kdmEAms9XljB_LqFb3za4ksQkjUY51zprEc/viewform?usp=send_form")
-        let request = NSURLRequest(URL: url!, cachePolicy: .ReturnCacheDataElseLoad, timeoutInterval: 5.0)
-        
-        //let session = NSURLSession.sharedSession()
-        
-        //session.dataTaskWithRequest(request)
+        let request = NSURLRequest(URL: url!, cachePolicy: .ReturnCacheDataElseLoad, timeoutInterval: 3.0)
         
         
+        
+        
+        /*
+        
+        let myURLTask = NSURLSession.sharedSession().dataTaskWithURL(url!)
+        {
+            
+            (myData, response, error) in
+            
+            if (error == nil)
+            {
+                let myHTMLString = NSString(data: myData!, encoding: NSUTF8StringEncoding)
+                self.myWebView.loadHTMLString(myHTMLString as! String, baseURL: nil)
+            
+            }
+        }
+        
+        myURLTask.resume()
+        
+        */
+
+        print("about to loadRequest")
         self.myWebView.loadRequest(request)
-        
-        
-        
        
+        
+        // Idea to show loading screen Icon
+        /*
+        
+        if(myWebView.loading == false){
+            print("loading")
+            let rect = CGRect(x: 50, y: 50, width: 100, height: 100)
+            
+            let label = UILabel(frame: rect)
+            label.text = "Loading"
+            label.textColor = UIColor(red: 1, green: 0, blue: 1, alpha: 1)
+            
+        }
+        */
+        
+    
+//        let config = NSURLSessionConfiguration.defaultSessionConfiguration()
+//        let delegate = NSURLSessionDelegate.self
+//        let session = NSURLSession.sharedSession()
+
+        
+        
+        
+/*      //Trying to impliment a URL Session and Cache
+        
+        
+        let url2 = NSURL(string: "https://itunes.apple.com")
+        let request2 = NSURLRequest(URL: url2!)
+        let config2 = NSURLSessionConfiguration.defaultSessionConfiguration()
+        let session2 = NSURLSession(configuration: config2)
+        
+        let task = session2.dataTaskWithRequest(request2, completionHandler: {(data, response, error) in
+            
+            // notice that I can omit the types of data, response and error
+            
+            
+            
+        });
+        
+        // do whatever you need with the task e.g. run
+        task.resume()
+        
+        self.myWebView.loadRequest(<#T##request: NSURLRequest##NSURLRequest#>)
+        
+        
+*/
+    
+        
+        
+        
         
         
         
